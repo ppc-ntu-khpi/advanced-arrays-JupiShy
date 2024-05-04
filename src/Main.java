01b1f5b4de47dd4ca15320a1130607e499e5e2f2
Sofiia Kyrychok <101671647+JupiShy@users.noreply.github.com>
2024-05-04 11:37:01 +0000

    done

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
+/**
+ * Клас, що заповнює матрицю розміру визначеного користувачем,
+ * випадковими значеннями від 0 до 9 включно,
+ * та визначає суму всіх елементів цієї матриці.
+ */
 public class Main {
 
     public static void main(String[] args) {
-        int [][] matrix = new int[3][3];
-        int x=0;
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
-        for(int[] row:matrix)
-            Arrays.fill(row,x);
+        System.out.print("Введіть кількість рядків (N): ");
+        int n = scanner.nextInt();
+        System.out.print("Введіть кількість стовпців (M): ");
+        int m = scanner.nextInt();
+        scanner.close();
 
-        for(int[] row:matrix)
+        int[][] matrix = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = random.nextInt(10);
+            }
+        }
+
+        for (int[] row : matrix)
             System.out.println(Arrays.toString(row));
+
+        int matrixElemSum = Arrays.stream(matrix)
+                .flatMapToInt(Arrays::stream)
+                .sum();
+
+        
+        System.out.println("Сума елементів матриці: " + matrixElemSum);
     }
 }
